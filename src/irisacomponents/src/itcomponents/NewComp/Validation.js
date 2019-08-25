ccc6ec3e95aa170f699429e352277724505f8a04
Mahdi Nabiollahi <mmahdi.nabiollahi@mehr.ui.ac.ir>
2019-08-25 14:23:50 +0430

    Update Validation.js

--- src/irisacomponents/src/itcomponents/NewComp/Validation.js
+++ src/irisacomponents/src/itcomponents/NewComp/Validation.js
@@ -1,225 +1,199 @@
-import validator from "validator";
-import moment from "moment-jalaali";
+import validator from 'validator'
+import moment from 'jalali-moment'
 
+function validation(text, validationType, validationTypeParam) {
+  //console.log(`${text} - ${validationType} - ${validationTypeParam}`)
 
-   function validation(text, validationType, validationTypeParam) {
-    //console.log(`${text} - ${validationType} - ${validationTypeParam}`)
+  if (typeof validationType === 'undefined' || validationType === null)
+    return true
 
-    if(typeof validationType==='undefined' || validationType===null)
-        return true
+  let result = {
+    isValid: true,
+    messages: { validationType: [], validationMessage: [] }
+  }
 
-    let result = {isValid:false,messages:{validationType:[],validationMessage:[]}}
+  for (let i = 0; i < validationType.length; i++) {
+    let msg = ''
+    let validity = true
 
-    for (let i = 0; i < validationType.length; i++) {
-        result.isValid = true;
-        let msg = "";
+    switch (validationType[i]) {
+      case 'afterDate': {
+        let targetDate = validationTypeParam
+          ? parseInt(validationTypeParam[i])
+          : new Date().getTime()
+        let displayDate = moment(new Date(targetDate))
+          .locale('fa')
+          .format('YYYY/MM/DD HH:mm:ss')
 
-        switch (validationType[i]) {
-            case "afterDate": {
-                let targetDate = validationTypeParam ? parseInt(validationTypeParam[i]) : new Date().getTime();
-                let displayDate = moment(new Date(targetDate)).locale('fa').format('YYYY/MM/DD HH:mm:ss');
+        validity = (!isNaN(text) ? parseInt(text) : text) >= targetDate
+        msg = 'زمان حداقل: ' + displayDate
 
-                result.isValid = (!isNaN(text) ? parseInt(text) : text) >= targetDate;
-                msg = "زمان حداقل: " + displayDate;
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+      case 'alpha': {
+        validity = validator.isAlpha(text, 'en-US')
+        msg = 'کاراکترهای مجاز: حروف لاتین'
 
-                break;
-            }
+        break
+      }
 
-            case "alpha": {
-                result.isValid =  validator.isAlpha(text, 'en-US');
-                msg = "کاراکترهای مجاز: حروف لاتین";
+      case 'alphaFa': {
+        let regEx = /^[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي]+[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي ]*$/
+        validity = validator.matches(validator.rtrim(text), regEx)
+        msg = 'کاراکترهای مجاز: حروف فارسی و فاصله'
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+        break
+      }
 
-                break;
-            }
+      case 'alphanumeric': {
+        validity = validator.isAlphanumeric(text, 'en-US')
+        msg = 'کاراکترهای مجاز: حروف لاتین و عدد'
 
-            case "alphaFa": {
-                let regEx = /^[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي]+[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي ]*$/;
-                result.isValid =  validator.matches(validator.rtrim(text), regEx);
-                msg = "کاراکترهای مجاز: حروف فارسی و فاصله";
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+      case 'alphanumericFa': {
+        let regEx = /^[٠١٢٣٤٥٦٧٨٩0-9آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي]+[٠١٢٣٤٥٦٧٨٩0-9آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي ]*$/
+        validity = validator.matches(validator.rtrim(text), regEx)
+        msg = 'کاراکترهای مجاز: حروف فارسی، فاصله و عدد'
 
-                break;
-            }
+        break
+      }
 
-            case "alphanumeric": {
-                result.isValid =  validator.isAlphanumeric(text, 'en-US');
-                msg = "کاراکترهای مجاز: حروف لاتین و عدد";
+      case 'equals': {
+        if (validationTypeParam && validationTypeParam[i]) {
+          validity = validator.equals(text, validationTypeParam[i])
+          msg = 'مقادیر مساوی نمی باشند'
+        }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+        break
+      }
 
-                break;
-            }
+      case 'email': {
+        validity = validator.isEmail(text)
+        msg = 'ایمیل معتبر نمی باشد'
 
-            case "alphanumericFa": {
-                let regEx = /^[٠١٢٣٤٥٦٧٨٩0-9آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي]+[٠١٢٣٤٥٦٧٨٩0-9آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیي ]*$/;
-                result.isValid =  validator.matches(validator.rtrim(text), regEx);
-                msg = "کاراکترهای مجاز: حروف فارسی، فاصله و عدد";
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+      case 'lowercase': {
+        validity = validator.isLowercase(text)
+        msg = 'کاراکترهای مجاز: حروف لاتین کوچک'
 
-                break;
-            }
+        break
+      }
 
-            case "equals": {
-                if (validationTypeParam && validationTypeParam[i]) {
-                    result.isValid =  validator.equals(text, validationTypeParam[i]);
-                    msg = "مقادیر مساوی نمی باشند";
-                }
+      case 'matches': {
+        if (validationTypeParam && validationTypeParam[i]) {
+          validity = validator.matches(text, validationTypeParam[i])
+          msg = 'ورودی با عبارت منطقی مطابقت ندارد'
+        }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+        break
+      }
 
-                break;
-            }
+      case 'mobilePhone': {
+        validity = validator.isMobilePhone(text, 'fa-IR')
+        msg = 'لطفا شماره موبایل وارد نمایید'
 
-            case "email": {
-                result.isValid =  validator.isEmail(text);
-                msg = "ایمیل معتبر نمی باشد";
- 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+        break
+      }
 
-                break;
-            }
+      case 'number': {
+        validity = validator.isNumeric(text)
+        msg = 'لطفا عدد وارد نمایید'
 
-            case "lowercase": {
-                result.isValid =  validator.isLowercase(text);
-                msg = "کاراکترهای مجاز: حروف لاتین کوچک";
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
-                
-                break;
-            }
+      case 'numberFloat': {
+        if (validationTypeParam && validationTypeParam[i]) {
+          validity = validator.isFloat(text, validationTypeParam[i])
+          msg = `لطفا عدد اعشاری وارد نمایید\n`
+          msg += validationTypeParam[i].min
+            ? `بزرگ تر یا مساوی: ${validationTypeParam[i].min}\n`
+            : ``
+          msg += validationTypeParam[i].max
+            ? `کوچک تر یا مساوی: ${validationTypeParam[i].max}\n`
+            : ``
+          msg += validationTypeParam[i].gt
+            ? `بزرگ تر از: ${validationTypeParam[i].gt}\n`
+            : ``
+          msg += validationTypeParam[i].lt
+            ? `کوچک تر از: ${validationTypeParam[i].lt}\n`
+            : ``
+        } else {
+          validity = validator.isFloat(text)
+          msg = `لطفا عدد اعشاری وارد نمایید\n`
+        }
 
-            case "matches": {
-                if (validationTypeParam && validationTypeParam[i]) {
-                    result.isValid =  validator.matches(text, validationTypeParam[i]);
-                    msg = "ورودی با عبارت منطقی مطابقت ندارد";
-                }
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+      case 'numberInt': {
+        if (validationTypeParam && validationTypeParam[i]) {
+          validity = validator.isInt(text, validationTypeParam[i])
 
-                break;
-            }
+          msg = `لطفا عدد صحیح وارد نمایید\n`
+          msg += validationTypeParam[i].min
+            ? `بزرگ تر یا مساوی: ${validationTypeParam[i].min}\n`
+            : ``
+          msg += validationTypeParam[i].max
+            ? `کوچک تر یا مساوی: ${validationTypeParam[i].max}\n`
+            : ``
+          msg += validationTypeParam[i].gt
+            ? `بزرگ تر از: ${validationTypeParam[i].gt}\n`
+            : ``
+          msg += validationTypeParam[i].lt
+            ? `کوچک تر از: ${validationTypeParam[i].lt}\n`
+            : ``
+        } else {
+          result.isValid = validator.isInt(text)
+          msg = `لطفا عدد صحیح وارد نمایید\n`
+        }
 
-            case "mobilePhone": {
-                result.isValid =  validator.isMobilePhone(text, 'fa-IR');
-                msg = "لطفا شماره موبایل وارد نمایید";
+        break
+      }
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+      case 'required': {
+        validity = !validator.isEmpty(text)
+        msg = 'مقدار فیلد اجباری می باشد'
 
-                break;
-            }
+        break
+      }
 
-            case "number": {
-                result.isValid =  validator.isNumeric(text);
-                msg = "لطفا عدد وارد نمایید";
+      case 'uppercase': {
+        validity = validator.isUppercase(text)
+        msg = 'کاراکترهای مجاز: حروف لاتین بزرگ'
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+        break
+      }
 
-                break;
-            }
+      default: {
+        validity = true
+      }
+    } //switch end
+    result.isValid = result.isValid && validity
 
-            case "numberFloat": {
-                if (validationTypeParam && validationTypeParam[i]) {
-                    result.isValid =  validator.isFloat(text, validationTypeParam[i]);
+    result.messages.validationType = [
+      ...result.messages.validationType,
+      validationType[i]
+    ]
 
-                    msg = `لطفا عدد اعشاری وارد نمایید\n`;
-                    msg += validationTypeParam[i].min
-                        ? `بزرگ تر یا مساوی: ${validationTypeParam[i].min}\n` : ``;
-                    msg += validationTypeParam[i].max
-                        ? `کوچک تر یا مساوی: ${validationTypeParam[i].max}\n` : ``;
-                    msg += validationTypeParam[i].gt
-                        ? `بزرگ تر از: ${validationTypeParam[i].gt}\n` : ``;
-                    msg += validationTypeParam[i].lt
-                        ? `کوچک تر از: ${validationTypeParam[i].lt}\n` : ``;
-                } else {
-                    result.isValid = validator.isFloat(text);
-                    msg = `لطفا عدد اعشاری وارد نمایید\n`;
-                }
+    if (!validity)
+      result.messages.validationMessage = [
+        ...result.messages.validationMessage,
+        '\n' + msg
+      ]
+  } //for end
 
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
+  //{isValid:false,messages:{validationType:[],validationParams:[]}}
 
-                break;
-            }
-
-            case "numberInt": {
-                if (validationTypeParam && validationTypeParam[i]) {
-                    result.isValid =  validator.isInt(text, validationTypeParam[i]);
-
-                    msg = `لطفا عدد صحیح وارد نمایید\n`;
-                    msg += validationTypeParam[i].min
-                        ? `بزرگ تر یا مساوی: ${validationTypeParam[i].min}\n` : ``;
-                    msg += validationTypeParam[i].max
-                        ? `کوچک تر یا مساوی: ${validationTypeParam[i].max}\n` : ``;
-                    msg += validationTypeParam[i].gt
-                        ? `بزرگ تر از: ${validationTypeParam[i].gt}\n` : ``;
-                    msg += validationTypeParam[i].lt
-                        ? `کوچک تر از: ${validationTypeParam[i].lt}\n` : ``;
-                } else {
-                    result.isValid =  validator.isInt(text);
-                    msg = `لطفا عدد صحیح وارد نمایید\n`;
-                }
-
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
-
-                break;
-            }
-
-            case "required": {
-                result.isValid =  !validator.isEmpty(text);
-                msg = "مقدار فیلد اجباری می باشد";
-
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
-
-                break;
-            }
-
-            case "uppercase": {
-                result.isValid =  validator.isUppercase(text);
-                msg = "کاراکترهای مجاز: حروف لاتین بزرگ";
-
-                result.messages.validationType = [...result.messages.validationType,validationType[i]]
-                result.messages.validationMessage = [...result.messages.validationMessage,msg]
-
-                break;
-            }
-
-            default: {
-                result.isValid = true;
-            }
-        }//switch end
-
-
-
-    }//for end
-
-    //{isValid:false,messages:{validationType:[],validationParams:[]}}
-
-    return {isValid:result.isValid,
-        messages:result.messages && result.messages.validationMessage ? result.messages.validationMessage.reduce((s,item)=>{return s+item+'\n'}):''
-    }
-};
-
-function validation1(){
-
+  return {
+    isValid: result.isValid,
+    messages: result.messages.validationMessage
+  }
 }
 
 export default validation
