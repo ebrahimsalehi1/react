a2eae1bdaf2800cad1c32050a913ea1f12921d6f
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-05-16 13:20:49 +0430

    changes

---
+++ src/Components/IrisaComponents/Mydate/Time.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+    mount,shallow
+} from '../../../../enzymeConfig'
+import IbxTimePicker2  from './IbxTimePicker2'
+
+describe('Test on Time component',()=>{
+    it('step1-',()=>{
+
+        const comp = shallow(<IbxTimePicker2 />)        
+
+        
+    })
+})
